5ae8cec8cda1f32c3225d9a51b82893965e9e5cf
johngruen <john.gruen@gmail.com>
2013-10-16 11:16:07 -0400

    finishing first pass on design template

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -9,12 +9,21 @@ $(function(){
 			p.animate({left:-0},100);
 			p.addClass('showing');
 			p.find("#palette_control").removeClass("icon-chevron-right").addClass('icon-chevron-left');
-
-			
-
 		}
 		
 	});
 
-	$("#date_slider").slider({ values: [ 1,99],range:true });
+	$("#date_slider").slider({ values: [ 5,95],range:true });
+
+	  $('select').each(function(){
+            var title = $(this).attr('title');
+            if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
+            $(this)
+                .css({'z-index':10,'opacity':0,'-khtml-appearance':'none'})
+                .after('<span class="select ">' + title + '<span class="icon-caret-down"></span></span>')
+                .change(function(){
+                    val = $('option:selected',this).text();
+                    $(this).next().text(val);
+                 })
+        });
 });
