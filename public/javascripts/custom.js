2dea23f61e42d21f0290bf753c8c14cb72f31f94
scheinercc <albert@scheiner.cc>
2014-01-24 07:54:25 +1100

    unify whitespace for indentation to tabs

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -7,18 +7,18 @@ $(function(){
 		if ( p.hasClass('showing')) {
 
 			p
-			    .animate({left:-200},100)
-			    .removeClass('showing')
-			    .find("#palette_control")
-			    .removeClass("icon-chevron-left")
-			    .addClass('icon-chevron-right');
+				.animate({left:-200},100)
+				.removeClass('showing')
+				.find("#palette_control")
+				.removeClass("icon-chevron-left")
+				.addClass('icon-chevron-right');
 		} else {
 			p
-			    .animate({left:-0},100)
-			    .addClass('showing')
-			    .find("#palette_control")
-			    .removeClass("icon-chevron-right")
-			    .addClass('icon-chevron-left');
+				.animate({left:-0},100)
+				.addClass('showing')
+				.find("#palette_control")
+				.removeClass("icon-chevron-right")
+				.addClass('icon-chevron-left');
 		}
 		
 	});
@@ -27,26 +27,26 @@ $(function(){
 
 	$('select').each( function() {
 
-            var title = $(this).attr('title');
+			var title = $(this).attr('title');
 
-            if ( $('option:selected', this).val() != ''  ) {
-            	title = $('option:selected',this).text();
-            }
+			if ( $('option:selected', this).val() != ''  ) {
+				title = $('option:selected',this).text();
+			}
 
-            $(this)
-                .css({
-                	'z-index': 10,
-                	'opacity': 0,
-                	'-khtml-appearance': 'none'
-                })
-                .after('<span class="select ">' + title + '<span class="icon-caret-down"></span></span>')
-                .change( function() {
+			$(this)
+				.css({
+					'z-index': 10,
+					'opacity': 0,
+					'-khtml-appearance': 'none'
+				})
+				.after('<span class="select ">' + title + '<span class="icon-caret-down"></span></span>')
+				.change( function() {
 
-                    var val = $('option:selected',this).text();
+					var val = $('option:selected',this).text();
 
-                    $(this)
-                    	.next()
-                    	.text( val );
-                 });
-        });
+					$(this)
+						.next()
+						.text( val );
+				 });
+		});
 });
