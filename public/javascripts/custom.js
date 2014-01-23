23f03e2715be93bd56ed9c7f13c279a6dc4776db
scheinercc <albert@scheiner.cc>
2014-01-24 07:50:00 +1100

    adjust indentation and spacing for readability

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,29 +1,52 @@
 $(function(){
+
 	var p = $("#palette");
-	$("#palette_toggle").click(function(){
-		if(p.hasClass('showing')) {
-			p.animate({left:-200},100);
-			p.removeClass('showing');
-			p.find("#palette_control").removeClass("icon-chevron-left").addClass('icon-chevron-right');
+
+	$("#palette_toggle").on( 'click', function() {
+
+		if ( p.hasClass('showing')) {
+
+			p
+			    .animate({left:-200},100)
+			    .removeClass('showing')
+			    .find("#palette_control")
+			    .removeClass("icon-chevron-left")
+			    .addClass('icon-chevron-right');
 		} else {
-			p.animate({left:-0},100);
-			p.addClass('showing');
-			p.find("#palette_control").removeClass("icon-chevron-right").addClass('icon-chevron-left');
+			p
+			    .animate({left:-0},100)
+			    .addClass('showing')
+			    .find("#palette_control")
+			    .removeClass("icon-chevron-right")
+			    .addClass('icon-chevron-left');
 		}
 		
 	});
 
-	$("#date_slider").slider({ values: [ 5,95],range:true });
+	$("#date_slider").slider({ values: [ 5, 95 ], range: true });
 
-	  $('select').each(function(){
+	$('select').each( function() {
+
             var title = $(this).attr('title');
-            if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
+
+            if ( $('option:selected', this).val() != ''  ) {
+            	title = $('option:selected',this).text();
+            }
+
             $(this)
-                .css({'z-index':10,'opacity':0,'-khtml-appearance':'none'})
+                .css({
+                	'z-index': 10,
+                	'opacity': 0,
+                	'-khtml-appearance': 'none'
+                })
                 .after('<span class="select ">' + title + '<span class="icon-caret-down"></span></span>')
-                .change(function(){
-                    val = $('option:selected',this).text();
-                    $(this).next().text(val);
-                 })
+                .change( function() {
+
+                    var val = $('option:selected',this).text();
+
+                    $(this)
+                    	.next()
+                    	.text( val );
+                 });
         });
 });
